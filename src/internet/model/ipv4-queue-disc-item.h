65543a7171f6c3fb86a465a65e4d1d7262fb6d24
Mohit P. Tahiliani <tahiliani@nitk.edu.in>
2016-08-18 14:46:34 +0530

    Fixed typos

--- src/internet/model/ipv4-queue-disc-item.h
+++ src/internet/model/ipv4-queue-disc-item.h
@@ -80,20 +80,20 @@ public:
   virtual bool GetUint8Value (Uint8Values field, uint8_t &value) const;
 
   /**
-   * \brief Checks whether the packet has ECT-0 or ECT-1 bits set 
-   * \returns true if the packet has ECT_0 or ECT-1 bits set, false otherwise
+   * \brief Checks whether the packet has ECN_ECT0 or ECN_ECT1 bits set 
+   * \return true if the packet has ECN_ECT0 or ECN_ECT1 bits set, false otherwise
    */
   bool IsEcnCapable (void) const;
 
   /**
-   * \brief Marks the packet by setting CE bits
-   * \returns true if the packet gets marked, false otherwise
+   * \brief Marks the packet by setting ECN_CE bits
+   * \return true if the packet gets marked, false otherwise
    */
   virtual bool Mark (void);
 
   /** 
-   * \brief Checks whether the packet is already marked with CE
-   * \returns true if the packet is already marked with CE, false otherwise
+   * \brief Checks whether the packet is already marked with ECN_CE
+   * \return true if the packet is already marked with ECN_CE, false otherwise
    */  
   virtual bool IsMarked (void) const;
 
